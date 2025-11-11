0e52a82ff4ce42f5306e9212135e510fcc65758f
IdrisK <145732466+Kubomu@users.noreply.github.com>
2025-11-11 22:46:55 +0300

    Update and rename postBuild.sh to postBuild

--- postBuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Disable Jupyter token authentication
-mkdir -p ~/.jupyter
-cat >> ~/.jupyter/jupyter_notebook_config.py << EOF
-c.NotebookApp.token = ''
-c.NotebookApp.password = ''
-c.NotebookApp.allow_origin = '*'
-EOF
